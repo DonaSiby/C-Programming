51a6ce9cf444e3d1e85119a31ed5763b4c9201ba
Dona Siby <76901510+DonaSiby@users.noreply.github.com>
2021-05-30 14:52:37 +0530

    Create Welcome.c

---
+++ Welcome.c
@@ -0,0 +1,13 @@
+/*************************************************************************************************
+Author:Dona Siby
+Title :Program to welcome to this world of c programming.
+Date  :22/05/2021
+*************************************************************************************************/
+#include<stdio.h>
+int main(){
+    char name[50];
+    printf("Enter your name :");
+    scanf("%s",name);
+    printf("Welcome to the world of c programming %s",name);
+    return 0;
+}
