afa036a18608dc61f630a199fe1b5d950f702338
Dona Siby <76901510+DonaSiby@users.noreply.github.com>
2021-05-30 15:14:43 +0530

    Create Type.c

---
+++ Type.c
@@ -0,0 +1,14 @@
+/*************************************************************************************************
+Author:Dona Siby
+Title :Type
+Date  :12/05/2021
+*************************************************************************************************/
+#include <stdio.h>
+int main(){
+    printf("int: %ld \n",sizeof(int));
+    printf("float: %ld \n",sizeof(float));
+    printf("double: %ld \n",sizeof(double));
+    printf("char: %ld \n",sizeof(char));
+
+    return 0;
+}
