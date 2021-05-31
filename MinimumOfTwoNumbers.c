f0247e97b595639e4909d555913f05e684d04cbc
Dona Siby <76901510+DonaSiby@users.noreply.github.com>
2021-05-31 20:05:57 +0530

    Create MinimumOfTwoNumbers.c

---
+++ MinimumOfTwoNumbers.c
@@ -0,0 +1,19 @@
+/*************************************************************************************************
+Author:Dona Siby
+Title :Program to find maximum of two numbers.(Conditional operator).
+Date  :31/05/2021
+*************************************************************************************************/
+#include<stdio.h>
+int main(){
+   int number1,number2,minimumNumber;
+   printf("Enter the first number :",number1);
+   scanf("%d",&number1);
+  
+   printf("Enter the second number :",number2);
+   scanf("%d",&number2);
+  
+   minimumNumber=number1<number2?number1:number2;
+   printf("The smallest number is  %d",minimumNumber);
+   return 0;
+}
+
