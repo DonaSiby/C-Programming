19a545eaa8755860a14cfef11f5210e47f188af8
Dona Siby <76901510+DonaSiby@users.noreply.github.com>
2021-05-31 19:54:20 +0530

    Create PositiveOrNegative.c

---
+++ PositiveOrNegative.c
@@ -0,0 +1,15 @@
+/*************************************************************************************************
+Author:Dona Siby
+Title :Positive or negative.(Conditional operator).
+Date  :31/05/2021
+*************************************************************************************************/
+#include<stdio.h>
+int main(){
+   int number;
+   printf("Enter the number :",number);
+   scanf("%d",&number);
+  
+  //conditional operator.
+   number>0?printf("The number is positive"):printf("The number is negative");
+   return 0;
+}
